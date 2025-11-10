6657b98b05ddb374ba914bc0830357672fce8474
Taeha Kim <taeha.kim@infradise.com>
2025-11-10 20:56:45 +0900

    feat(cluster): add cluster client base interface

--- lib/valkey_client.dart
+++ lib/valkey_client.dart
@@ -11,6 +11,9 @@ export 'valkey_pool.dart';
 // Export cluster info data classes (v1.2.0)
 export 'src/cluster_info.dart';
 
+// Export the new cluster client interface (v1.3.0)
+export 'valkey_cluster_client_base.dart';
+
 // Export public exception classes (v1.0.0)
 export 'src/exceptions.dart';
 
