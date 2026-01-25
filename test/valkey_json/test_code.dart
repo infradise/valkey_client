c03d66fbc8c5c4c18f9d1319e9dbb21d21aafd24
Taeha Kim <taeha.kim@infradise.com>
2026-01-25 17:30:57 +0900

    test: add test (jsonArrAppend, jsonClear, jsonMGet, jsonMSet, jsonNumIncrBy, jsonNumMultBy)

--- test/valkey_json/test_code.dart
+++ test/valkey_json/test_code.dart
@@ -98,34 +98,44 @@ void main() async {
     expect(result, equals([])); // Expected Usage
   });
 
-  // test('jsonArrAppend - normal array', () async {
-  //   await client.jsonSet(key: 'json:arrappend', path: '.', data: '["x"]');
-  //   final len = await client.jsonArrAppend(
-  //       key: 'json:arrappend', path: '.', values: '"y"');
-  //   expect(len, equals(2));
-  //   final result = await client.jsonGet(key: 'json:arrappend', path: '.');
-  //   expect(result, equals('["x","y"]'));
-  // });
+  test('jsonArrAppend - normal array', () async {
+    await client.jsonSet(key: 'json:arrappend', path: '.', data: '["x"]');
+    final len = await client.jsonArrAppend(
+        key: 'json:arrappend',
+        path: '.',
+        // (X) values: '"y"');
+        values: ['y']);
+    expect(len, equals(2));
+    final result = await client.jsonGet(key: 'json:arrappend', path: '.');
+    // (X) expect(result, equals('["x","y"]'));
+    expect(result, equals(['x', 'y']));
+  });
 
-  // test('jsonArrAppend - not an array', () async {
-  //   await client.jsonSet(key: 'json:notarray', path: '.', data: '{"a":1}');
-  //   try {
-  //     await client.jsonArrAppend(
-  //         key: 'json:notarray', path: '.', values: '"b"');
-  //     fail('Should throw an error');
-  //   } catch (e) {
-  //     expect(e, isA<ValkeyException>());
-  //     expect((e as ValkeyException).message,
-  //         equals('WRONGTYPE JSON element is not an array'));
-  //   }
-  // });
+  test('jsonArrAppend - not an array', () async {
+    await client.jsonSet(key: 'json:notarray', path: '.', data: '{"a":1}');
+    try {
+      await client.jsonArrAppend(
+          key: 'json:notarray',
+          path: '.',
+          // (X) values: '"b"');
+          values: ['b']);
+      fail('Should throw an error');
+    } catch (e) {
+      expect(e, isA<ValkeyException>());
+      expect((e as ValkeyException).message,
+          equals('WRONGTYPE JSON element is not an array'));
+    }
+  });
 
-  // test('jsonArrAppend - empty array', () async {
-  //   await client.jsonSet(key: 'json:emptyarr2', path: '.', data: '[]');
-  //   final len = await client.jsonArrAppend(
-  //       key: 'json:emptyarr2', path: '.', values: '"foo"');
-  //   expect(len, equals(1));
-  // });
+  test('jsonArrAppend - empty array', () async {
+    await client.jsonSet(key: 'json:emptyarr2', path: '.', data: '[]');
+    final len = await client.jsonArrAppend(
+        key: 'json:emptyarr2',
+        path: '.',
+        // (X) values: '"foo"');
+        values: ['foo']);
+    expect(len, equals(1));
+  });
 
   test('jsonArrIndex - value exists', () async {
     await client.jsonSet(key: 'json:arridx', path: '.', data: '["a","b","c"]');
@@ -269,51 +279,52 @@ void main() async {
     }
   });
 
-  // test('jsonClear - object', () async {
-  //   await client.jsonSet(
-  //       key: 'json:clearobj', path: '.', data: '{"a":1,"b":2}');
-  //   final cleared = await client.jsonClear(key: 'json:clearobj', path: '.');
-  //   expect(cleared, greaterThanOrEqualTo(1));
-  //   final result = await client.jsonGet(key: 'json:clearobj', path: '.');
-  //   expect(result, equals('{}'));
-  // });
+  test('jsonClear - object', () async {
+    await client.jsonSet(
+        key: 'json:clearobj', path: '.', data: '{"a":1,"b":2}');
+    final cleared = await client.jsonClear(key: 'json:clearobj', path: '.');
+    expect(cleared, greaterThanOrEqualTo(1));
+    final result = await client.jsonGet(key: 'json:clearobj', path: '.');
+    // (X) expect(result, equals('{}'));
+    expect(result, equals({}));
+  });
 
-  // test('jsonClear - array', () async {
-  //   await client.jsonSet(
-  //       key: 'json:cleararr', path: '.', data: '["a","b"]');
-  //   final cleared = await client.jsonClear(key: 'json:cleararr', path: '.');
-  //   expect(cleared, greaterThanOrEqualTo(1));
-  //   final result = await client.jsonGet(key: 'json:cleararr', path: '.');
-  //   expect(result, equals('[]'));
-  // });
+  test('jsonClear - array', () async {
+    await client.jsonSet(key: 'json:cleararr', path: '.', data: '["a","b"]');
+    final cleared = await client.jsonClear(key: 'json:cleararr', path: '.');
+    expect(cleared, greaterThanOrEqualTo(1));
+    final result = await client.jsonGet(key: 'json:cleararr', path: '.');
+    // (X) expect(result, equals('[]'));
+    expect(result, equals([]));
+  });
 
-  // test(
-  //     'jsonClear - json string (not container) and '
-  //          'returns json empty string (It is a bug)',
-  //     () async {
-  //   await client.jsonSet(
-  //       key: 'json:clearnotcontainer', path: '.', data: '"foo"');
-  //   final cleared =
-  //       await client.jsonClear(key: 'json:clearnotcontainer', path: '.');
-  //   final result =
-  //       await client.jsonGet(key: 'json:clearnotcontainer', path: '.');
-  //   expect(cleared, equals(1));
-  //   expect(result, equals('""'));
-  // });
+  test(
+      'jsonClear - json string (not container) and '
+      'returns json empty string (It is a bug)', () async {
+    await client.jsonSet(
+        key: 'json:clearnotcontainer', path: '.', data: '"foo"');
+    final cleared =
+        await client.jsonClear(key: 'json:clearnotcontainer', path: '.');
+    final result =
+        await client.jsonGet(key: 'json:clearnotcontainer', path: '.');
+    expect(cleared, equals(1));
+    // (X) expect(result, equals('""'));
+    expect(result, equals(''));
+  });
 
-  // test(
-  //     'jsonClear - json empty string (not container) and '
-  //                'returns empty json string (It is a bug)',
-  //     () async {
-  //   await client.jsonSet(
-  //       key: 'json:clearnotcontainer', path: '.', data: '"foo"');
-  //   final cleared =
-  //       await client.jsonClear(key: 'json:clearnotcontainer', path: '.');
-  //   final result =
-  //       await client.jsonGet(key: 'json:clearnotcontainer', path: '.');
-  //   expect(cleared, equals(1));
-  //   expect(result, equals('""'));
-  // });
+  test(
+      'jsonClear - json empty string (not container) and '
+      'returns empty json string (It is a bug)', () async {
+    await client.jsonSet(
+        key: 'json:clearnotcontainer', path: '.', data: '"foo"');
+    final cleared =
+        await client.jsonClear(key: 'json:clearnotcontainer', path: '.');
+    final result =
+        await client.jsonGet(key: 'json:clearnotcontainer', path: '.');
+    expect(cleared, equals(1));
+    // expect(result, equals('""'));
+    expect(result, equals(''));
+  });
 
   test('jsonDel - normal', () async {
     await client.jsonSet(key: 'json:del', path: '.', data: '{"a":1,"b":2}');
@@ -352,91 +363,125 @@ void main() async {
     expect(result2, containsPair('a', 3)); // Expected Usage
   });
 
-  // test('jsonMget - multiple keys', () async {
-  //   await client.jsonSet(key: 'json:mget1', path: '.', value: '{"a":1}');
-  //   await client.jsonSet(key: 'json:mget2', path: '.', value: '{"a":2}');
-  //   final vals =
-  //       await client.jsonMget(
-  //           keys: ['json:mget1', 'json:mget2'], path: '.a');
-  //   expect(vals, isA<List>());
-  //   expect(vals.length, equals(2));
-  //   expect(vals[0], equals('1'));
-  //   expect(vals[1], equals('2'));
-  // });
+  test('jsonMGet - multiple keys', () async {
+    await client.jsonSet(key: 'json:mget1', path: '.', data: '{"a":1}');
+    await client.jsonSet(key: 'json:mget2', path: '.', data: '{"a":2}');
+    final vals =
+        await client.jsonMGet(keys: ['json:mget1', 'json:mget2'], path: '.a');
+    expect(vals, isA<List>());
+    expect(vals.length, equals(2));
+    // (X) expect(vals[0], equals('1'));
+    expect(vals[0], equals(1));
+    // (X) expect(vals[1], equals('2'));
+    expect(vals[1], equals(2));
+  });
 
-  // test('jsonMget - some keys missing', () async {
-  //   await client.jsonSet(key: 'json:mget3', path: '.', value: '{"a":1}');
-  //   final vals =
-  //       await client.jsonMget(
-  //           keys: ['json:mget3', 'json:missing'], path: '.a');
-  //   expect(vals.length, equals(2));
-  //   expect(vals[0], equals('1'));
-  //   expect(vals[1], isNull);
-  // });
+  test('jsonMGet - some keys missing', () async {
+    await client.jsonSet(key: 'json:mget3', path: '.', data: '{"a":1}');
+    final vals =
+        await client.jsonMGet(keys: ['json:mget3', 'json:missing'], path: '.a');
+    expect(vals.length, equals(2));
+    // (X) expect(vals[0], equals('1'));
+    expect(vals[0], equals(1));
+    expect(vals[1], isNull);
+  });
 
-  // test('jsonMset - multiple paths', () async {
-  //   // It is same with jsonSet when Mset just set one key-path-value
-  //   await client.jsonMset(
-  //     entries: [
-  //       (key: 'json:mset', path: '.', value: '{"a":999,"b":[999]}'),
-  //     ],
-  //   );
-  //   final result1 = await client.jsonGet(key: 'json:mset', path: '.');
-  //   await client.jsonMset(
-  //     entries: [
-  //       (key: 'json:mset', path: '.a', value: '0'),
-  //       (key: 'json:mset', path: '.b', value: '0'),
-  //     ],
-  //   );
-  //   final result2 = await client.jsonGet(key: 'json:mset', path: '.');
-  //   await client.jsonMset(
-  //     entries: [
-  //       (key: 'json:mset', path: '.b', value: 'false'),
-  //       (key: 'json:mset', path: '.c', value: 'true'),
-  //       (key: 'json:mset', path: '.d', value: 'null'),
-  //     ],
-  //   );
-  //   final result3 = await client.jsonGet(key: 'json:mset', path: '.');
+  test('jsonMSet - multiple paths', () async {
+    // It is same with jsonSet when Mset just set one key-path-value
+    await client.jsonMSet(
+      entries: [
+        // (X) (key: 'json:mset', path: '.', value: '{"a":999,"b":[999]}'),
+        const JsonMSetEntry(key: 'json:mset', path: '.', value: {
+          'a': 999,
+          'b': [999]
+        }),
+      ],
+    );
 
-  //   // It is same with jsonSet when Mset just set one key-path-value
-  //   await client.jsonMset(
-  //     entries: [
-  //       (key: 'json:mset', path: '.', value: '{"z":1,"y":2}'),
-  //     ],
-  //   );
-  //   final result4 = await client.jsonGet(key: 'json:mset', path: '.');
-  //   expect(result1, equals('{"a":999,"b":[999]}'));
-  //   expect(result2, equals('{"a":0,"b":0}'));
-  //   expect(result3, equals('{"a":0,"b":false,"c":true,"d":null}'));
-  //   expect(result4, equals('{"z":1,"y":2}'));
-  // });
+    final result1 = await client.jsonGet(key: 'json:mset', path: '.');
+    await client.jsonMSet(
+      entries: [
+        // (X) (key: 'json:mset', path: '.a', value: '0'),
+        const JsonMSetEntry(key: 'json:mset', path: '.a', value: 0),
+        // (X) (key: 'json:mset', path: '.b', value: '0'),
+        const JsonMSetEntry(key: 'json:mset', path: '.b', value: 0),
+      ],
+    );
 
-  // test('jsonNumincrby - normal', () async {
-  //   await client.jsonSet(key: 'json:numincr', path: '.', value: '{"n":10}');
-  //   final newVal =
-  //       await client.jsonNumincrby(
-  //           key: 'json:numincr', path: '.n', value: 5);
-  //   expect(newVal, contains('15'));
-  // });
+    final result2 = await client.jsonGet(key: 'json:mset', path: '.');
+    await client.jsonMSet(
+      entries: [
+        // (X) (key: 'json:mset', path: '.b', value: 'false'),
+        const JsonMSetEntry(key: 'json:mset', path: '.b', value: false),
+        // (X) (key: 'json:mset', path: '.c', value: 'true'),
+        const JsonMSetEntry(key: 'json:mset', path: '.c', value: true),
+        // (X) (key: 'json:mset', path: '.d', value: 'null'),
+        const JsonMSetEntry(key: 'json:mset', path: '.d', value: null),
+      ],
+    );
+    final result3 = await client.jsonGet(key: 'json:mset', path: '.');
 
-  // test('jsonNumincrby - not a number', () async {
-  //   await client.jsonSet(
-  //       key: 'json:numincr2', path: '.', value: '{"n":"foo"}');
-  //   try {
-  //     await client.jsonNumincrby(key: 'json:numincr2', path: '.n', value: 1);
-  //     fail('Should throw');
-  //   } catch (e) {
-  //     expect(e, isA<Exception>());
-  //   }
-  // });
+    // It is same with jsonSet when Mset just set one key-path-value
+    await client.jsonMSet(
+      entries: [
+        // (X) (key: 'json:mset', path: '.', value: '{"z":1,"y":2}'),
+        const JsonMSetEntry(
+            key: 'json:mset', path: '.', value: {'z': 1, 'y': 2}),
+      ],
+    );
+    final result4 = await client.jsonGet(key: 'json:mset', path: '.');
+    // (X) expect(result1, equals('{"a":999,"b":[999]}'));
+    expect(
+        result1,
+        equals({
+          'a': 999,
+          'b': [999]
+        }));
+    // (X) expect(result2, equals('{"a":0,"b":0}'));
+    expect(result2, equals({'a': 0, 'b': 0}));
+    // (X) expect(result3, equals('{"a":0,"b":false,"c":true,"d":null}'));
+    expect(result3, equals({'a': 0, 'b': false, 'c': true, 'd': null}));
+    // (X) expect(result4, equals('{"z":1,"y":2}'));
+    expect(result4, equals({'z': 1, 'y': 2}));
+  });
 
-  // test('jsonNummultby - normal', () async {
-  //   await client.jsonSet(key: 'json:nummult', path: '.', value: '{"n":2}');
-  //   final newVal =
-  //       await client.jsonNummultby(
-  //           key: 'json:nummult', path: '.n', value: 3);
-  //   expect(newVal, contains('6'));
-  // });
+  test('jsonNumIncrBy - normal', () async {
+    await client.jsonSet(
+        key: 'json:numincr',
+        path: '.',
+        // (X) data: '{"n":10}');
+        data: {'n': 10}); // Expected usage
+    final newVal =
+        await client.jsonNumIncrBy(key: 'json:numincr', path: '.n', value: 5);
+    // (X) expect(newVal, contains('15'));
+    expect([newVal], contains(15)); // Expected usage
+    expect(newVal, equals(15)); // Expected usage
+  });
+
+  test('jsonNumIncrBy - not a number', () async {
+    await client.jsonSet(
+        key: 'json:numincr2',
+        path: '.',
+        // (X) data: '{"n":"foo"}');
+        data: {'n': 'foo'}); // Expected usage
+
+    try {
+      await client.jsonNumIncrBy(key: 'json:numincr2', path: '.n', value: 1);
+      fail('Should throw');
+    } catch (e) {
+      expect(e, isA<Exception>());
+      // rethrow; // Uncomment to see the exception
+    }
+  });
+
+  test('jsonNumMultBy - normal', () async {
+    await client.jsonSet(key: 'json:nummult', path: '.', data: '{"n":2}');
+    final newVal =
+        await client.jsonNumMultBy(key: 'json:nummult', path: '.n', value: 3);
+    // (X) expect(newVal, contains('6'));
+    expect([newVal], contains(6)); // Expected usage
+    expect(newVal, equals(6)); // Expected usage
+  });
 
   // test('jsonObjkeys - object', () async {
   //   await client.jsonSet(
